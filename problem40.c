64e9b0207258d4d34699fcf20908e108f68224e6
vivek dixit <dixitvivek040707@gmail.com>
2026-06-14 11:46:01 +0530

    Refactor palindrome check and print lowercase string
    Modified palindrome check to convert string to lowercase and print it.

---
+++ problem40.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+int main() {
+    char str[100];
+    scanf("%s", str);
+    for(int i = 0; i < strlen(str); i++) {
+        str[i] = tolower(str[i]);
+    }
+    printf("%s", str);
+    return 0;
+}
