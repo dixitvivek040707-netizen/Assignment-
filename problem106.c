291f0f8c948d80425ed503e4d0db409c4834d0ce
vivek dixit <dixitvivek040707@gmail.com>
2026-06-29 18:06:33 +0530

    Modify main function to concatenate two strings
    Replaced Harshad number logic with string concatenation.

---
+++ problem106.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    char str1[100], str2[100];
+    scanf("%s %s", str1, str2);
+    int i = 0;
+    while(str1[i] != '\0') i++;
+    int j = 0;
+    while(str2[j] != '\0') {
+        str1[i] = str2[j];
+        i++; j++;
+    }
+    str1[i] = '\0';
+    printf("%s", str1);
+    return 0;
+}
