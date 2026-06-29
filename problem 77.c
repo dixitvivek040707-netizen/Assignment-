8d68f9c6f79dd7c6a4cb4449712272445ebb94b9
vivek dixit <dixitvivek040707@gmail.com>
2026-06-29 17:48:48 +0530

    Add prime number checker and print first n primes
    Implement a function to check for prime numbers and modify main to print the first n prime numbers.

---
+++ problem 77.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int isPrime(int n) {
+    if(n <= 1) return 0;
+    if(n == 2) return 1;
+    if(n % 2 == 0) return 0;
+    for(int i = 3; i*i <= n; i += 2) {
+        if(n % i == 0) return 0;
+    }
+    return 1;
+}
+int main() {
+    int n, count = 0, num = 2;
+    scanf("%d", &n);
+    while(count < n) {
+        if(isPrime(num)) {
+            printf("%d ", num);
+            count++;
+        }
+        num++;
+    }
+    return 0;
+}
