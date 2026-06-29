324bdffb3e42fe11ea6370cebfd953921c9b0157
vivek dixit <dixitvivek040707@gmail.com>
2026-06-29 18:06:54 +0530

    Change automorphic number logic to string comparison
    Replaced automorphic number check with string comparison of two inputs.

---
+++ problem107.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    char str1[100], str2[100];
+    scanf("%s %s", str1, str2);
+    int i = 0, flag = 1;
+    while(str1[i] != '\0' || str2[i] != '\0') {
+        if(str1[i] != str2[i]) {
+            flag = 0;
+            break;
+        }
+        i++;
+    }
+    if(flag) printf("Equal");
+    else printf("Not Equal");
+    return 0;
+}
