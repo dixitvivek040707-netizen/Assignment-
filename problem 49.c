10a58a0bc0d88157acf551b4e2e3a716c0d9b0d9
vivek dixit <dixitvivek040707@gmail.com>
2026-06-29 17:19:01 +0530

    Implement swap function and modify main logic
    Removed string length calculation and added swap function.

---
+++ problem 49.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void swap(int *a, int *b) {
+    *a = *a + *b;
+    *b = *a - *b;
+    *a = *a - *b;
+}
+int main() {
+    int x, y;
+    scanf("%d %d", &x, &y);
+    swap(&x, &y);
+    printf("%d %d", x, y);
+    return 0;
+}
