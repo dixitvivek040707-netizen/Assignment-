c12c04dd11b9b4ee5d0ae17c8aa1bb447d91e208
vivek dixit <dixitvivek040707@gmail.com>
2026-06-29 17:37:05 +0530

    Refactor code to calculate average instead of transpose
    Removed the transpose function and added functionality to calculate the average of user-input numbers.

---
+++ problem75.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    float sum = 0;
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        sum += arr[i];
+    }
+    printf("%.2f", sum / n);
+    return 0;
+}
