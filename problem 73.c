5e4cb29575d33f3114ac5516ff54e2130dbb9fdf
vivek dixit <dixitvivek040707@gmail.com>
2026-06-29 17:36:06 +0530

    Calculate and print the maximum value in array
    Replaced reverse print with max value calculation.

--- problem 73.c
+++ problem 73.c
@@ -4,8 +4,10 @@ int main() {
     scanf("%d", &n);
     int arr[n];
     for(int i = 0; i < n; i++) scanf("%d", &arr[i]);
-    for(int i = n-1; i >= 0; i--) {
-        printf("%d ", arr[i]);
+    int max = arr[0];
+    for(int i = 1; i < n; i++) {
+        if(arr[i] > max) max = arr[i];
     }
+    printf("%d", max);
     return 0;
 }
