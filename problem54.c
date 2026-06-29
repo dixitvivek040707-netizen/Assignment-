f83fba7763bf84eb7366202096d4583959501ec0
vivek dixit <dixitvivek040707@gmail.com>
2026-06-29 17:22:04 +0530

    Implement maxArray function and update main
    Added a function to find the maximum value in an array and modified main to read an array from user input.

---
+++ problem54.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int maxArray(int arr[], int n) {
+    int max = arr[0];
+    for(int i = 1; i < n; i++) {
+        if(arr[i] > max) max = arr[i];
+    }
+    return max;
+}
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for(int i = 0; i < n; i++) scanf("%d", &arr[i]);
+    printf("%d", maxArray(arr, n));
+    return 0;
+}
